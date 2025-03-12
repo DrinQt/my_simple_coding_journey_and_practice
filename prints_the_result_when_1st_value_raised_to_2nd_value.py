fabc0000763ae1750bec7ac66b13bf6d098f9e4f
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 00:42:53 +0800

    pseudocode

---
+++ prints_the_result_when_1st_value_raised_to_2nd_value.py
@@ -0,0 +1,5 @@
+#print the programs purpose
+#get the first value from the user
+# get the second value from the user
+# calculate the result of the first value raised to the power of the second value
+# print the result
