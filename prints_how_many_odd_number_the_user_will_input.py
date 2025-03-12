e848d6e40c0ea142b3c7843a6a7383b3f9ceafde
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 01:12:57 +0800

    pseudocode

---
+++ prints_how_many_odd_number_the_user_will_input.py
@@ -0,0 +1,4 @@
+#print the programs purpose
+#initialize the variable to store the number of odd numbers
+#use a for loop to iterate through the range of numbers the user inputs and if the number is odd, add 1 to the variable storing the number of odd numbers
+#print the number of odd numbers the user inputted
