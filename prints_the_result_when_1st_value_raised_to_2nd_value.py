97e03d64ed54297d835ebe8eb1cece3ee9817fb0
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 00:49:44 +0800

    uses ** operation to get and print the formatted answer

--- prints_the_result_when_1st_value_raised_to_2nd_value.py
+++ prints_the_result_when_1st_value_raised_to_2nd_value.py
@@ -8,4 +8,7 @@ num1 = float(input("Enter the base number (first number): "))
 num2 = float(input("Enter the exponent number (second number): "))
 
 # calculate the result of the first value raised to the power of the second value
+result = num1 ** num2
+
 # print the result
+print(f"{num1} raised to the power of {num2} is: {result}")
