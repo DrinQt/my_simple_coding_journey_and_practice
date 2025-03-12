0c823e89942325836ee6f28bdb431e188bbb3126
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 01:17:52 +0800

    prints the program purpose and initialize the variable to store odd number count

--- prints_how_many_odd_number_the_user_will_input.py
+++ prints_how_many_odd_number_the_user_will_input.py
@@ -1,4 +1,9 @@
 #print the programs purpose
+print("Input 10 numerical values, this program will print how many odd numbers are in there.")
+
 #initialize the variable to store the number of odd numbers
+odd_count = 0
+
 #use a for loop to iterate through the range of numbers the user inputs and if the number is odd, add 1 to the variable storing the number of odd numbers
+
 #print the number of odd numbers the user inputted
