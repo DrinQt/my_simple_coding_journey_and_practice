f3c13aea62072d1a73d3f3ff85468adf67630e3a
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-12 22:38:51 +0800

    uses max function to print the higher number between 2 values

--- sum_of_2_number_getter.py
+++ sum_of_2_number_getter.py
@@ -1,12 +1,14 @@
 # tell what the program is about and ask user to input 2 values
+# set the input variable into a float to handle values with decimal points
 print("This program determines the higher value between 2 numerical values.")
 
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
-
-# set the input variable into a float to handle values with decimal points
 # Compare value using 'max' function
 # print the higher value
+
+print("The higher value is", max(num1, num2))
+
 
 
