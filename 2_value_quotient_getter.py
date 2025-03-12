1fa7c3427ad7c813339f431d8f23d0e9b0ca014e
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 00:27:01 +0800

    prints the programs purpose and ask the user for denominator and numerator

--- 2_value_quotient_getter.py
+++ 2_value_quotient_getter.py
@@ -1,5 +1,11 @@
 #print the programs purpose
+print("This program gets the quotient of two numerical values.")
+
 #prompt the user to enter a numerator
+num1 = float(input("Enter the numerator: "))
+
 #prompt the user to enter a denominator
+num2 = float(input("Enter the denominator: "))
+
 #calculate the quotient of the numerator and denominator
 #display the quotient of the numerator and denominator
