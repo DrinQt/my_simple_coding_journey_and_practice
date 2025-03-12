cc6e2c569393ca23aad014f8e0ef0cd20b2c91d6
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 00:33:29 +0800

    calculate the quotient using / operator and if else function to check if denominator is zero

--- 2_value_quotient_getter.py
+++ 2_value_quotient_getter.py
@@ -7,5 +7,10 @@ num1 = float(input("Enter the numerator: "))
 #prompt the user to enter a denominator
 num2 = float(input("Enter the denominator: "))
 
-#calculate the quotient of the numerator and denominator
+#calculate the quotient of the numerator and denominator use if statement to check if the denominator is zero
+if num2 != 0:
+    quotient = num1 / num2
 #display the quotient of the numerator and denominator
+    print("The quotient of the two numbers is:", quotient)
+else:
+    print("Error: Division by zero is not allowed.")
