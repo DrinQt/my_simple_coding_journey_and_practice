ee7bc4170758e62474e0ceabb5b88d27d5e8707e
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 01:36:47 +0800

    use for loop and if function to check if number is even. Print number

--- prints_all_even_number_from_0_to_100.py
+++ prints_all_even_number_from_0_to_100.py
@@ -1,4 +1,9 @@
 #print the programs purpose
-#use for loop to print all even numbers from 0 to 100
-#use if statement to check if the number is even
+print("This program will print all the even numbers starting from 0 to 100.")
+
+#use for loop to print all even numbers from 0 to 100. Use if statement to check if the number is even
+for number in range(0, 101):
+    if number % 2 == 0:
+        print(number)
+
 #use print statement to print the number
