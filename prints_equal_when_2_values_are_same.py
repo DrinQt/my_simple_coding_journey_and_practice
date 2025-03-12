da0283ae789234bffd841d980d585d16b6a87d6f
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-12 23:26:49 +0800

    pseudocode ft. prints the prrogram purpose and ask the user 2 values

---
+++ prints_equal_when_2_values_are_same.py
@@ -0,0 +1,10 @@
+#tell the user what this program does
+#get the 2 values from the user using float() function to calibrate decimals aswell
+print("This program determines whether the two values are equal or not.")
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+#use if statement to check if the 2 values are equal
+#use print() function to print the values are equal
+
