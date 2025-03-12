24e222cacac86f4266865c7b4093b2208e4ff528
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 01:02:09 +0800

    print purpose, initialized sum variable to 0 and uses for loop to iterate the user to input 10 times

--- get_the_sum_of_all_10_values.py
+++ get_the_sum_of_all_10_values.py
@@ -1,5 +1,12 @@
 #print the programs purpose
+print("This program sums up all 10 numerical values.")
+
 #initialize the sum variable to 0
+total_sum = 0 
+
 #use for loop to iterate 10 times and prompt the user to enter a number
+for i in range(10):
+    number = float(input(f"Enter number {i + 1}: "))
+    
 #add the number to the sum
 #print the sum of all 10 numbers
