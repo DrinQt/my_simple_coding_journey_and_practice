5b4b8bb54480bd7984bad592d83757ff71bdfdd0
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 01:06:16 +0800

    add total_sum += number in the function to print the sum of all 10 number

--- get_the_sum_of_all_10_values.py
+++ get_the_sum_of_all_10_values.py
@@ -7,6 +7,7 @@ total_sum = 0
 #use for loop to iterate 10 times and prompt the user to enter a number
 for i in range(10):
     number = float(input(f"Enter number {i + 1}: "))
+    total_sum += number
     
-#add the number to the sum
 #print the sum of all 10 numbers
+print("The sum of all the numbers is:", total_sum)
