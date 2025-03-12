97022fb4facbebb773da7533caf1163952be4270
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 01:47:35 +0800

    Use for loop and if statement to iterate and check the number if it satisfies the purpose.Print it.

--- print_all_number_from_0_to_100_without_number_ending_in_zero.py
+++ print_all_number_from_0_to_100_without_number_ending_in_zero.py
@@ -1,4 +1,9 @@
 #print the programs purpose
+print("This program will print all numbers between 0 and 100 except numbers ending in zero.")
+
 #use a for loop to iterate through the numbers 0 to 100
 #use an if statement to check if the number ends in 0. Use if not to check if the number does not end in 0.
 #use the print() function to print the number
+for number in range(101):
+    if number % 10 != 0:
+        print(number)
