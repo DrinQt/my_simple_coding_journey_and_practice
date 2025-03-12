3de417ad8bcc51a4e48b718fbbaa2d9a0b0e9c29
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-12 23:45:35 +0800

    pseudocode

---
+++ 2_value_sum_getter.py
@@ -0,0 +1,5 @@
+#print the purpose of the program
+#ask user to input 2 values
+#set the input variable into a float to handle values with decimal points
+#add num1 + num2
+#print the sum of the two values
