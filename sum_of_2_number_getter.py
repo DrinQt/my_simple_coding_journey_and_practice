f93663e4ec680d5d408e4a974cf6c4d4d9d85f96
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-12 17:05:21 +0800

    tells what the program is about and asks user to input 2 values

---
+++ sum_of_2_number_getter.py
@@ -0,0 +1,12 @@
+# tell what the program is about and ask user to input 2 values
+print("This program determines the higher value between 2 numerical values.")
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+
+# set the input variable into a float to handle values with decimal points
+# Compare value using 'max' function
+# print the higher value
+
+
