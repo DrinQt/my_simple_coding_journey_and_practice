65af2ebb68d064c3c6517be7f68d2b173c343ba7
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 00:56:32 +0800

    pseudocode

---
+++ get_the_sum_of_all_10_values.py
@@ -0,0 +1,5 @@
+#print the programs purpose
+#initialize the sum variable to 0
+#use for loop to iterate 10 times and prompt the user to enter a number
+#add the number to the sum
+#print the sum of all 10 numbers
