d25ba33006797fb6362e1904dbd7fb7631ee8f82
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-12 23:59:14 +0800

    prints the program purpose and asks the user to input 2 values

--- 2_value_product_getter.py
+++ 2_value_product_getter.py
@@ -1,4 +1,8 @@
 #print the programs purpose
 #ask the user to input 2 values
+print("This program gets the product of two numerical values.")
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
 #multiply the 2 values
 #print the product of the 2 values
