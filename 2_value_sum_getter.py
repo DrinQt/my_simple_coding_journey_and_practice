b0eabe8ddc3c7fc50e7aebaf1a8504d2ad1877ff
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-12 23:49:15 +0800

    prints the programs purpose and asks the user to put 2 values

--- 2_value_sum_getter.py
+++ 2_value_sum_getter.py
@@ -1,5 +1,9 @@
 #print the purpose of the program
 #ask user to input 2 values
 #set the input variable into a float to handle values with decimal points
+print("This program gets the sum of two numerical values.")
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
 #add num1 + num2
 #print the sum of the two values
