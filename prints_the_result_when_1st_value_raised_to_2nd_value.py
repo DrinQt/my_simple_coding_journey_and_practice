4f7d52caa432abd0a740d21fe85b3f718cdfabf3
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 00:46:55 +0800

    prints the programs purpose and ask the user to input base and exponent number

--- prints_the_result_when_1st_value_raised_to_2nd_value.py
+++ prints_the_result_when_1st_value_raised_to_2nd_value.py
@@ -1,5 +1,11 @@
 #print the programs purpose
+print("This program calculates the result of the first value raised to the power of the second value")
+
 #get the first value from the user
+num1 = float(input("Enter the base number (first number): "))
+
 # get the second value from the user
+num2 = float(input("Enter the exponent number (second number): "))
+
 # calculate the result of the first value raised to the power of the second value
 # print the result
