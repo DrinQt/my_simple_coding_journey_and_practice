0b21a75d9083a6f9778176ce3cb076c82db73748
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 00:19:31 +0800

    pseudocode

---
+++ 2_value_quotient_getter.py
@@ -0,0 +1,5 @@
+#print the programs purpose
+#prompt the user to enter a numerator
+#prompt the user to enter a denominator
+#calculate the quotient of the numerator and denominator
+#display the quotient of the numerator and denominator
