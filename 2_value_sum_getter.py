c8ab66bc0ba582a1cab726065a0d83511279b3fc
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-12 23:52:08 +0800

    uses the + function to get and print the sum of the values

--- 2_value_sum_getter.py
+++ 2_value_sum_getter.py
@@ -5,5 +5,9 @@ print("This program gets the sum of two numerical values.")
 
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
+
 #add num1 + num2
+sum_of_numbers = num1 + num2
+
 #print the sum of the two values
+print("The sum of the two numbers is:", sum_of_numbers)
