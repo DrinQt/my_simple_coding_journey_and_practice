57937a6bc46b8446394a740b6f002c4ac69451a4
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 01:23:19 +0800

    Use for loop to iterate through the range of numbers the user can input and checks if the number is odd add 1 to variable odd_count. Then print

--- prints_how_many_odd_number_the_user_will_input.py
+++ prints_how_many_odd_number_the_user_will_input.py
@@ -5,5 +5,12 @@ print("Input 10 numerical values, this program will print how many odd numbers a
 odd_count = 0
 
 #use a for loop to iterate through the range of numbers the user inputs and if the number is odd, add 1 to the variable storing the number of odd numbers
+for i in range(10):
+    number = float(input(f"Enter number {i + 1}: "))
+    if number % 2 != 0:
+        odd_count += 1
 
 #print the number of odd numbers the user inputted
+print("The number of odd numbers is:", odd_count)
+
+
