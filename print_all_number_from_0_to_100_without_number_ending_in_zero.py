e2a8113d914f3e07870f31e25da51de287048edb
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 01:43:09 +0800

    pseudocode

---
+++ print_all_number_from_0_to_100_without_number_ending_in_zero.py
@@ -0,0 +1,4 @@
+#print the programs purpose
+#use a for loop to iterate through the numbers 0 to 100
+#use an if statement to check if the number ends in 0. Use if not to check if the number does not end in 0.
+#use the print() function to print the number
