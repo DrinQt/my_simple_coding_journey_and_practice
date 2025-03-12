7cecaa771fe6101b1098cfe9af3e908c83eef2f0
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 00:01:31 +0800

    multiply values using * function and prints the product

--- 2_value_product_getter.py
+++ 2_value_product_getter.py
@@ -4,5 +4,9 @@ print("This program gets the product of two numerical values.")
 
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
+
 #multiply the 2 values
 #print the product of the 2 values
+product_of_numbers = num1 * num2
+
+print("The product of the two numbers is:", product_of_numbers)
