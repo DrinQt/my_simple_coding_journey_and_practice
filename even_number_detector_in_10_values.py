d053e99c199b11135f7a501fc1ff1eaf19ca0ccb
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 14:49:07 +0800

    print purpose, set even_count variable to 0

--- even_number_detector_in_10_values.py
+++ even_number_detector_in_10_values.py
@@ -1,5 +1,10 @@
 #print the program purpose
+print("This program counts how many even numbers you will input.")
+
 #set even number counter to 0
+even_count = 0
+
 #use for loop to iterate 10 times and 'if' statement to check if the number is even.
 #If the number is even, increment the even number counter by 1
+
 #print the even number counter
