3b44b4d438672109230691f0357fe0452a04a75a
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 12:51:42 +0800

    prints the purpose of the program and ask the user to put 2 values

--- 2_value_difference_getter.py
+++ 2_value_difference_getter.py
@@ -1,5 +1,12 @@
 #print the program purpose
+print("This program gets the difference of two numerical values.")
+
 #prompt the user to enter the first value
+num1 = float(input("Enter the first number: "))
+
 #prompt the user to enter the second value
+num2 = float(input("Enter the second number: "))
+
 #compute the difference between the two values using '-' operator
+
 #print the difference
