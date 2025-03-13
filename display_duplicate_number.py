55c25831f7c007a6b8ce070289ebf83733b2a68d
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-14 00:54:40 +0800

    prompt user to put 10 values using 'for' and range function

--- display_duplicate_number.py
+++ display_duplicate_number.py
@@ -5,7 +5,10 @@ print("This program will display duplicate numbers in a list of 10 numbers")
 numbers = []
 
 #prompt the user to enter 10 numbers
-
+for i in range(10):
+    num = int(input(f"Enter number {i + 1}: "))
+    numbers.append(num)
+    
 #loop through the list to check for duplicate numbers
 
 #find the duplicate numbers and display them
