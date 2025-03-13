4a9a051cb71a8b303fb85f658548e35cd6e3a1c5
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 22:43:24 +0800

    print purpose, initialize number list

--- display_10_number_without_duplicate.py
+++ display_10_number_without_duplicate.py
@@ -1,5 +1,13 @@
 #print the program purpose
+print("This program will display 10 numbers without duplicate")
+
+#initialize the number list
+number_list = []
+
 #prompt the user to enter 10 numbers
+
+
 #find the unique numbers from the list
+
 #display the unique numbers
 
