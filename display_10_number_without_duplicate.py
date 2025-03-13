9b5fb17ff60c7622eb8eeb37b918506c84e2c860
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 22:59:04 +0800

    find unique_number and display it

--- display_10_number_without_duplicate.py
+++ display_10_number_without_duplicate.py
@@ -7,9 +7,13 @@ number_list = []
 #prompt the user to enter 10 numbers
 for i in range(10):
     num = int(input(f"Enter number {i + 1}: "))
-    numbers.append(num)
+    number_list.append(num)
+
 
 #find the unique numbers from the list
+unique_numbers = [num for num in number_list if number_list.count(num) == 1]
 
 #display the unique numbers
+print("Numbers without duplicates:", unique_numbers)
+
 
