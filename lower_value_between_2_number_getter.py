5eeb627a4da7837efd7ed4a9858d304bd77e1e2d
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 11:01:55 +0800

    prints the programs purpose and asks the user to put 2 values

--- lower_value_between_2_number_getter.py
+++ lower_value_between_2_number_getter.py
@@ -1,4 +1,10 @@
 #print the purpose of the program
+print("This program determines the smaller value between two numerical values.")
+
 #prompt the user to enter the first number using float data type
+num1 = float(input("Enter the first number: "))
+
 #prompt the user to enter the second number using float data type
+num2 = float(input("Enter the second number: "))
+
 #print the lower value between the two numbers using 'min' function
