bddc2a273b327588317bf8a0b83e6162c2cf4719
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 23:41:36 +0800

    pseudocode

---
+++ unique_or_duplicate.py
@@ -0,0 +1,6 @@
+#print program purpose
+#initialize set to keep track of entered numbers
+#use exit condition to stop the loop
+#convert input to integer
+#check if number is unique or duplicate
+#print the result
