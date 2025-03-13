a585cca8f87a5cb07932687c3b8c60552db193e3
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-14 00:23:48 +0800

    print purpose, initialize variable

--- lowest_and_highest_value.py
+++ lowest_and_highest_value.py
@@ -1,5 +1,9 @@
 #print the purpose of the program
+print("This program will print the lowest and highest number you enter.")   
+
 #initialize a list to store the numbers
+numbers = []
+
 #use while true loop to get the input from the user
 #sort the list in ascending order
 #print the number from lowest to highest
