a757acc69ce0a5fa4b1328bf25854849e2cdef2e
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 13:15:56 +0800

    print program purpose and ask the user 2 values to divide

--- 2_value_int_quotient_getter.py
+++ 2_value_int_quotient_getter.py
@@ -1,5 +1,13 @@
 #print the purpose of the program
+print("This program gets the quotient of two numerical values without a decimal point.")
+
 #ask the user for the first number
+num1 = int(input("Enter the first number: "))
+
 #ask the user for the second number
+num2 = int(input("Enter the second number: "))
+
 #divide the first number by the second number using '//' operator
+
+
 #print the quotient
