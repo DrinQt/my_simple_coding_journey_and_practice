9a73b3706c39bc15397814b78c1455027e09f7f9
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 15:22:09 +0800

    print purpose, set 'num' variable to 0

--- print_number_0_to_100_without_ending_0_and_five.py
+++ print_number_0_to_100_without_ending_0_and_five.py
@@ -1,5 +1,11 @@
 #print the purpose of the program
+print("This program will print all numbers from 0 to 100 except numbers ending in zero or five.")
+
 #set 'num' to 0
+num = 0
+
 #while 'num' is less than 100 use if statement to check if 'num' is not equal to 0 and 'num' is not divisible by 5
+
 #print 'num'
+
 #increment 'num' by 1
