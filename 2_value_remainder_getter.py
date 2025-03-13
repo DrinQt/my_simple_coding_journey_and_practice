dd84403176b466838883d035a7e2e6930ab60fa5
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 13:36:44 +0800

    pseudocode

---
+++ 2_value_remainder_getter.py
@@ -0,0 +1,5 @@
+#print the purpose of the program
+#prompt the user to enter a number
+#prompt the user to enter a divisor
+#calculate the remainder of the division using the modulus operator
+#print the remainder
