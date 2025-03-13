d0a03531bb1a370e4ddb5c81646df76ccccc324b
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-14 01:54:15 +0800

    purpose and initialized variable total and count to 0

--- average_getter_unli_input.py
+++ average_getter_unli_input.py
@@ -1,4 +1,9 @@
 #print purpose of the program
+print("This program calculates the average of numbers entered by the user.")
+
 #Set the variable 'total' to 0 and 'count' to 0
+total = 0
+count = 0
+
 #prompt the user to enter a number
 #calculate and display avegare of the numbers entered
