5b227d44ef5444b1e97dcab562fa5f4f9e5dcb16
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 15:19:03 +0800

    pseudocode

---
+++ print_number_0_to_100_without_ending_0_and_five.py
@@ -0,0 +1,5 @@
+#print the purpose of the program
+#set 'num' to 0
+#while 'num' is less than 100 use if statement to check if 'num' is not equal to 0 and 'num' is not divisible by 5
+#print 'num'
+#increment 'num' by 1
