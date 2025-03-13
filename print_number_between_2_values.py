198b98f2e2dea19300f8d9213bca9bf412eb014d
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 15:37:53 +0800

    use 'if' to check condition and 'for' to loop print the values between

--- print_number_between_2_values.py
+++ print_number_between_2_values.py
@@ -7,3 +7,10 @@ end = int(input("Enter the ending number: "))
 
 #use if condition to check if the number is between the range and vice versa
 #use for loop to print the numbers between the range
+if start == end:
+    print("There are no numbers between the two values since they are equal.")
+else:
+    lower = min(start, end)
+    upper = max(start, end)
+    for num in range(lower + 1, upper):
+        print(num)
