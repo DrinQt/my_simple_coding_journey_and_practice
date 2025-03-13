00af2e4c11c8ba86158bafe1acc248a7c3c0e1b7
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 14:35:32 +0800

    print the purpose of the program and prompt user to put 10 values

--- print_the_result_of_1st_number_-_all_remaininng_number.py
+++ print_the_result_of_1st_number_-_all_remaininng_number.py
@@ -1,5 +1,11 @@
 #print the purpose of the program
+print("This program prints the result when you subtract each of the next nine values from the first value.")
+
 #prompt the user to enter the first number and use range function to get the remaining numbers
+numbers = [int(input(f"Enter number {i + 1}: ")) for i in range(10)]
+
 #set the first number to a variable 0
+
 #use a for loop to iterate through the remaining numbers
+
 #subtract the first number from the remaining numbers
