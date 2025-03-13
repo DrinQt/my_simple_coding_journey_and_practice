f5cf63610414370388abff64ba348286d1b4da97
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 15:35:56 +0800

    print purpose, get user input

--- print_number_between_2_values.py
+++ print_number_between_2_values.py
@@ -1,4 +1,9 @@
 #print the program purpose
+print("This program will print all numbers between the two values you input, excluding the values themselves.")
+
 #use 'start' and 'end' to get the range of numbers
+start = int(input("Enter the starting number: "))
+end = int(input("Enter the ending number: "))
+
 #use if condition to check if the number is between the range and vice versa
 #use for loop to print the numbers between the range
