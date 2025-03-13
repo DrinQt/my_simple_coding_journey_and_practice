afd6c93cb3e3791cc8ac14f3f0beefda616580aa
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 15:05:22 +0800

    print purpose, set 'num' to 0

--- print_all_odd_number_from_0_to_100.py
+++ print_all_odd_number_from_0_to_100.py
@@ -1,3 +1,7 @@
 #print program purpose
+print("This program will print all the odd numbers from 0 to 100.")
+
 #set 'num' variable to 1
+num = 1
+
 #while 'num' is less than 100 print 'num' then add 2 to 'num'
