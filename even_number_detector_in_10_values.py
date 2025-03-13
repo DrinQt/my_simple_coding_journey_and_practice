31c398b30fcd99e8d8fe5f90b34d36905fd54ce7
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 14:45:42 +0800

    pseudocode

---
+++ even_number_detector_in_10_values.py
@@ -0,0 +1,5 @@
+#print the program purpose
+#set even number counter to 0
+#use for loop to iterate 10 times and 'if' statement to check if the number is even.
+#If the number is even, increment the even number counter by 1
+#print the even number counter
