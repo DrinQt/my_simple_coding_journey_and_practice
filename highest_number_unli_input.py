8b26d0f0fad4282d1ddb7311cb44cc813adcda75
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-14 01:28:03 +0800

    print purpose, initialize highest number to 0

--- highest_number_unli_input.py
+++ highest_number_unli_input.py
@@ -1,5 +1,11 @@
 #print the program purpose
+print("This program will display the highest number you entered.")
+
 #set variable to store the highest number
+highest_number = None
+
 #prompt the user to enter a number
+
 #update the highest number
+
 #display the highest number if valid
