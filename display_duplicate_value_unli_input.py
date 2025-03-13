4ab87ce52c7a1d1f4b5e6cefd03e48c23f526570
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-14 01:13:20 +0800

    prompt user to put values

--- display_duplicate_value_unli_input.py
+++ display_duplicate_value_unli_input.py
@@ -5,5 +5,17 @@ print("This program finds the value with the most duplicates in a list of number
 number_count = {}
 
 #prompt user to enter number
+while True:
+    user_input = input("Enter a number (or type 'exit' to quit): ")
+
+    if user_input.lower() == 'exit':
+        break
+
+    try:
+        number = int(user_input)
+        number_count[number] = number_count.get(number, 0) + 1
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+
 #find value with the most duplicates and print it
 
