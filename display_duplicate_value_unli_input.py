a162d7ca0ff20640801b59d014baae7a2f743311
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-14 01:10:52 +0800

    print purpose, initialize number count

--- display_duplicate_value_unli_input.py
+++ display_duplicate_value_unli_input.py
@@ -1,5 +1,9 @@
 #print program purpose
+print("This program finds the value with the most duplicates in a list of numbers.")
+
 #initialize number count
+number_count = {}
+
 #prompt user to enter number
 #find value with the most duplicates and print it
 
