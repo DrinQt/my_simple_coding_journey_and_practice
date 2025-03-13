6b22a1b7b186989cdf2ceef8a99fb6a5a52cbce9
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 15:24:53 +0800

    use 'while' and 'if' to check values. print 'num' and increment it by 1

--- print_number_0_to_100_without_ending_0_and_five.py
+++ print_number_0_to_100_without_ending_0_and_five.py
@@ -5,7 +5,10 @@ print("This program will print all numbers from 0 to 100 except numbers ending i
 num = 0
 
 #while 'num' is less than 100 use if statement to check if 'num' is not equal to 0 and 'num' is not divisible by 5
-
 #print 'num'
-
 #increment 'num' by 1
+while num <= 100:
+    if num % 10 != 0 and num % 10 != 5:
+        print(num)
+    num += 1
+    
