af413657b870a0dbe60e166e24ef09b8b0f9f188
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 14:29:42 +0800

    pseudocode

---
+++ print_the_result_of_1st_number_-_all_remaininng_number.py
@@ -0,0 +1,5 @@
+#print the purpose of the program
+#prompt the user to enter the first number and use range function to get the remaining numbers
+#set the first number to a variable 0
+#use a for loop to iterate through the remaining numbers
+#subtract the first number from the remaining numbers
