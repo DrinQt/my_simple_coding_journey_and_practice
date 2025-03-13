596f2276da88d85cc1d0afacd6b2dab363942ab8
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 22:49:51 +0800

    use 'for' and range function. Also used 'number.append'.

--- display_10_number_without_duplicate.py
+++ display_10_number_without_duplicate.py
@@ -5,7 +5,9 @@ print("This program will display 10 numbers without duplicate")
 number_list = []
 
 #prompt the user to enter 10 numbers
-
+for i in range(10):
+    num = int(input(f"Enter number {i + 1}: "))
+    numbers.append(num)
 
 #find the unique numbers from the list
 
