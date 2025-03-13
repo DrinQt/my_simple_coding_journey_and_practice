e674a74367985276c2d419bbdafa64148d1a3e62
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-14 00:48:52 +0800

    pseudocode

---
+++ display_duplicate_number.py
@@ -0,0 +1,5 @@
+#print program purpose
+#initialize a list to store the numbers
+#prompt the user to enter 10 numbers
+#loop through the list to check for duplicate numbers
+#find the duplicate numbers and display them
