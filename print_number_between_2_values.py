949ad8762119f41a904f96de6db743c4a084475c
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 15:31:58 +0800

    pseudocode

---
+++ print_number_between_2_values.py
@@ -0,0 +1,4 @@
+#print the program purpose
+#use 'start' and 'end' to get the range of numbers
+#use if condition to check if the number is between the range and vice versa
+#use for loop to print the numbers between the range
