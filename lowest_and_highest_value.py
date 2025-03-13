6245441fe18d409d746325f150e4c4dbe82c8e0d
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-14 00:21:59 +0800

    pseudocode

---
+++ lowest_and_highest_value.py
@@ -0,0 +1,5 @@
+#print the purpose of the program
+#initialize a list to store the numbers
+#use while true loop to get the input from the user
+#sort the list in ascending order
+#print the number from lowest to highest
