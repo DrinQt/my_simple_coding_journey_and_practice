f4c0993ff3b635799801077d56cfa54f09b66fc8
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 14:52:35 +0800

    use for loop to  iterate, 'if' to check. Print the result

--- even_number_detector_in_10_values.py
+++ even_number_detector_in_10_values.py
@@ -6,5 +6,10 @@ even_count = 0
 
 #use for loop to iterate 10 times and 'if' statement to check if the number is even.
 #If the number is even, increment the even number counter by 1
+for i in range(10):
+    num = int(input(f"Enter number {i + 1}: "))
+    if num % 2 == 0:
+        even_count += 1
 
 #print the even number counter
+print("There are", even_count, "even numbers.")
