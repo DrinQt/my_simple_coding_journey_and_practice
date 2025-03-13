33f8a7221921f2acf176d2d38192d73b13b95ee6
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 11:34:06 +0800

    pseudocode

---
+++ 2_value_difference_getter.py
@@ -0,0 +1,5 @@
+#print the program purpose
+#prompt the user to enter the first value
+#prompt the user to enter the second value
+#compute the difference between the two values using '-' operator
+#print the difference
