56c1f98bbf20d0aad1964039398b83d0188cf2d1
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-14 01:45:05 +0800

    purpose and initialize variable to 0

--- highest_to_lowest_unli_input.py
+++ highest_to_lowest_unli_input.py
@@ -1,5 +1,10 @@
 #print the purpose of the program
+print("This program will sort numbers in descending order")
+
 #set number variable to empty list
+numbers = []
+
 #prompt user to enter a number
+
 #sort and print the number in descending order
 
