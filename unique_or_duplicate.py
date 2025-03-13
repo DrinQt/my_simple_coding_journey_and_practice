70f7242eb5db20e267c6d33a49869890c5149db1
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 23:44:53 +0800

    print purpose, initialize entered number set

--- unique_or_duplicate.py
+++ unique_or_duplicate.py
@@ -1,5 +1,9 @@
 #print program purpose
+print("This program will determine if a number is unique or duplicate.")
+
 #initialize set to keep track of entered numbers
+entered_numbers = set()
+
 #use exit condition to stop the loop
 #convert input to integer
 #check if number is unique or duplicate
