bea71f51df2899087464d4f5a3b92cc7b98bef93
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 10:44:59 +0800

    pseudocode

---
+++ lower_value_between_2_number_getter.py
@@ -0,0 +1,4 @@
+#print the purpose of the program
+#prompt the user to enter the first number using float data type
+#prompt the user to enter the second number using float data type
+#print the lower value between the two numbers using 'min' function
