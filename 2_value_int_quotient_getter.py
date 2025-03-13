15fc1aa0fbf4ef06a59c9869ef85848f3cf07c4b
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-13 13:09:28 +0800

    pseudocode

---
+++ 2_value_int_quotient_getter.py
@@ -0,0 +1,5 @@
+#print the purpose of the program
+#ask the user for the first number
+#ask the user for the second number
+#divide the first number by the second number using '//' operator
+#print the quotient
