81c694b65af011429039d7b57edc73572ebaa8fb
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-14 00:05:11 +0800

    print purpose, initialize variable

--- lowest_number_display_unli_input.py
+++ lowest_number_display_unli_input.py
@@ -1,5 +1,9 @@
 #print the program purpose
+print("This program will display the lowest number from the input numbers.")
+
 #initialize lowest number to 0
+lowest_number = None
+
 #initialize the loop to True
 #update the lowest number
 #display the lowest number if valid
