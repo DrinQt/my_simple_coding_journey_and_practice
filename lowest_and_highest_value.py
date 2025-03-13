ad17b08fe7fcdbd723c696eb376a680f44fce4e9
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-14 00:26:50 +0800

    use while true loop to collect input from the user

--- lowest_and_highest_value.py
+++ lowest_and_highest_value.py
@@ -5,5 +5,13 @@ print("This program will print the lowest and highest number you enter.")
 numbers = []
 
 #use while true loop to get the input from the user
+while True:
+    number = input("Enter a number or type 'done' to see the result: ")
+    #check if the user entered 'done'
+    if number == 'done':
+        break
+    #add the number to the list
+    numbers.append(int(number))
+
 #sort the list in ascending order
 #print the number from lowest to highest
