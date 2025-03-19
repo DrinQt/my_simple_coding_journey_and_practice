622d61568a21cc6d02b28c342f40f9212d25af97
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-19 14:35:18 +0800

    purpose and prompt user to input name

--- name_proper_casing_converter.py
+++ name_proper_casing_converter.py
@@ -1,3 +1,7 @@
 #print the purpose of the program
+print("This program converts a name to proper casing")
+
 #prompt the user to enter a name
+name = input("Enter a name: ")
+
 #convert and print the name to proper casing
