7729c7cba21314f9c8fff92fd3657d34cdfeaeee
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-19 14:03:53 +0800

    print purpose and prompt user to put name

--- name_to_capital_letter_converter.py
+++ name_to_capital_letter_converter.py
@@ -1,3 +1,7 @@
 #print the purpose of the program
+print("This program converts a name to capital letters")
+
 #prompt the user to enter a name
+name = input("Enter your full name: ")
+
 #convert the name to capital letters
