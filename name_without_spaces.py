d4d5ea5855cd11f2f6f0d8167fea9a23f3024bab
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-19 13:24:50 +0800

    print purpose and ask for input

--- name_without_spaces.py
+++ name_without_spaces.py
@@ -1,4 +1,9 @@
 #print purpose of the program
+print("This program removes leading and trailing spaces from a name")
+
 #get the name of the user
+full_name = input("Please enter your full name: ")
+
 #remove leading and trailing spaces
+
 #print the name without leading and trailing spaces
