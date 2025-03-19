bc6e6613a4c373ba3da7b24e4899119470c42576
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-19 13:29:11 +0800

    print name without traling spaces

--- name_without_spaces.py
+++ name_without_spaces.py
@@ -4,6 +4,5 @@ print("This program removes leading and trailing spaces from a name")
 #get the name of the user
 full_name = input("Please enter your full name: ")
 
-#remove leading and trailing spaces
-
 #print the name without leading and trailing spaces
+print(full_name.lstrip())
