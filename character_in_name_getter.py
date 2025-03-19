7ea33b63a2c52665a989d84a05ed783138ccfa4e
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-19 15:25:35 +0800

    print purpose, get the user name

--- character_in_name_getter.py
+++ character_in_name_getter.py
@@ -1,4 +1,9 @@
 #print the purpose of the program
+print("This program will count the number of characters in your name")
+
 #get the name of the user
+name = input("Please enter your name: ")
+
 #count the number of characters in the name
+
 #display the number of characters in the name
