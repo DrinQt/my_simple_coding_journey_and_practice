b9aaa6c9855309b76c6e1d997a228e8d860157f5
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-19 15:40:47 +0800

    program purpose and prompt user to enter name

--- name_to_pascal_case.py
+++ name_to_pascal_case.py
@@ -1,4 +1,9 @@
 #print the purpose of the program
+print("This program converts a name to pascal case")
+
 #ask the user for a name
+name = input("Enter a name: ")
+
 #convert the name to pascal case
+
 #print the name in pascal case
