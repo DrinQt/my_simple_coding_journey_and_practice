d220ce5602fc8afb4149a7d505cc1b88f48619f5
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-19 15:09:47 +0800

    print purpose and prompt user to enter sentence

--- word_counter.py
+++ word_counter.py
@@ -1,4 +1,8 @@
 #print the purpose of the program
+print("This program counts the number of words in a sentence.")
+
 #prompt the user to enter a sentence
+sentence = input("Enter a sentence: ")
+
 #count the number of words in the sentence
 #display the number of words in the sentence
