9a23677f17f56521fe4cca6a63486d80ad9374fd
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-19 15:54:10 +0800

    print purpose, get the name

--- name_to_snake_case.py
+++ name_to_snake_case.py
@@ -1,5 +1,10 @@
 #print the purpose of the program
+print("This program converts a name to snake case")
+
 #prompt the user to enter a name
+name = input("Enter a name: ")
+
 #convert the name to snake case
+
 #display the name in snake case
 
