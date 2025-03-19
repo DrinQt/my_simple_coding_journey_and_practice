57117532d89cc9a597d02457767660a80425ce07
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-19 15:31:58 +0800

    count character and print

--- character_in_name_getter.py
+++ character_in_name_getter.py
@@ -5,5 +5,7 @@ print("This program will count the number of characters in your name")
 name = input("Please enter your name: ")
 
 #count the number of characters in the name
+character_count = len(name)
 
 #display the number of characters in the name
+print("The number of characters in your name is", character_count)
