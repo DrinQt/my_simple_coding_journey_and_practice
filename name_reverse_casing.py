df2dabf964a0820b6a416281debd305d589e9927
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-19 14:53:17 +0800

    print purpose and prompt to put name

--- name_reverse_casing.py
+++ name_reverse_casing.py
@@ -1,5 +1,10 @@
 #print the purpose of the program
+print("This program will reverse the casing of your name.") 
+
 #ask the user to enter their name
+name = input("Enter your name: ")
+
 #reverse the casing of the name
+
 #print the reversed casing of the name
 
