18936cea62ac58227cdc6aa71672d75ad8d61074
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-19 13:49:29 +0800

    convert input in 6 digit format

--- 6_digit_format_converter.py
+++ 6_digit_format_converter.py
@@ -5,4 +5,8 @@ print("This program converts a number to a 6 digit format")
 number = int(input("Please enter a number (0-1000): "))
 
 #convert and print the number in 6 digit format
+if 0 <= number <= 1000:
+    print(f"{number:06}")
+else:
+    print("The number is not in the range 0-1000")
 
