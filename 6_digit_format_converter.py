a28b2a10c031668c0f9f086570c86d42bf3c1d6e
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-19 13:44:08 +0800

    print purpose, ask user to input number

--- 6_digit_format_converter.py
+++ 6_digit_format_converter.py
@@ -1,3 +1,8 @@
 #print purpose of the program
+print("This program converts a number to a 6 digit format")
+
 #ask user  to input 0-1000
+number = int(input("Please enter a number (0-1000): "))
+
 #convert and print the number in 6 digit format
+
