10b0fa1c465f3bb376f0498b946f864de9e12e64
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-19 14:14:18 +0800

    print purpose, prompt user to input name

--- name_to_lowercase.py
+++ name_to_lowercase.py
@@ -1,3 +1,7 @@
 #print the purpose of the program
+print("This program converts a name to lowercase.")
+
 #ask the user to enter a name
+name = input("Enter a name: ")
+
 #convert and print the name to lowercase
