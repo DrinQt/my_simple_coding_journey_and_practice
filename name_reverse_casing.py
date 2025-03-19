2e1d46722160a2319629ef93cf6d5281c9865e7b
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-19 14:57:29 +0800

    reverse the casing and print result

--- name_reverse_casing.py
+++ name_reverse_casing.py
@@ -5,6 +5,8 @@ print("This program will reverse the casing of your name.")
 name = input("Enter your name: ")
 
 #reverse the casing of the name
+reversed_casing = ''.join(char.swapcase() for char in name)
 
 #print the reversed casing of the name
+print(reversed_casing)
 
