b3ef73331d5eebfcf14073f080e6f707045643f8
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-03-29 00:29:15 +0800

    use custom_center and print result

--- center_without_using_centre_functions.py
+++ center_without_using_centre_functions.py
@@ -1,2 +1,7 @@
 #define custom center function
+def custom_center(s, width):
+    total_spaces = width - len(s)
+    return ' ' * (total_spaces // 2) + s + ' ' * (total_spaces - total_spaces // 2)
+
 #print center function
+print(custom_center("Hello", 11))
