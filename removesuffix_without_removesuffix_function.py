e02d0c5564fb57f2a4e629d400b20477c9b6eb27
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-04-01 18:53:46 +0800

    custom remove_suffix

--- removesuffix_without_removesuffix_function.py
+++ removesuffix_without_removesuffix_function.py
@@ -1,2 +1,7 @@
 #use custom function to remove suffix from string
+def custom_removesuffix(s, suffix):
+    if s.endswith(suffix):
+        return s[:-len(suffix)]
+    return s
+
 #Print the string without the suffix
