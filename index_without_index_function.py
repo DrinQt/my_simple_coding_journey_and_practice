49a801d1499f572254937601d37c6ed353e57f02
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-04-02 23:36:23 +0800

    custom_index

--- index_without_index_function.py
+++ index_without_index_function.py
@@ -1,2 +1,8 @@
 #use custom index function
+def custom_index(s, sub):
+    sub_len = len(sub)
+    for i in range(len(s) - sub_len + 1):
+        if s[i:i + sub_len] == sub:
+            return i
+    raise ValueError(f"'{sub}' is not in string")
 #print results
