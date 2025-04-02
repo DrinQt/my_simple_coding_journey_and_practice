9b49cb5a04a72a0185b75f56de4a62ef2a757c6f
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-04-02 22:47:13 +0800

    use custom_islower

--- islower_without_islower_function.py
+++ islower_without_islower_function.py
@@ -1,2 +1,8 @@
 #use custom function to check if a string is lowercase
+def custom_islower(s):
+    for char in s:
+        if 'A' <= char <= 'Z':
+            return False
+    return len(s) > 0
+
 #print result
