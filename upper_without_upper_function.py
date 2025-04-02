1f293e3e8400cbbb081871563f63570b8f31f892
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-04-02 22:24:58 +0800

    used custom_upper

--- upper_without_upper_function.py
+++ upper_without_upper_function.py
@@ -1,2 +1,11 @@
 #use custom function to convert string to upper case
+def custom_upper(s):
+    result = ""
+    for char in s:
+        if 'a' <= char <= 'z':
+            result += chr(ord(char) - 32)
+        else:
+            result += char
+    return result
+
 #print result
