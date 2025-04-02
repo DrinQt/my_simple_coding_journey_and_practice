757aa59c2b5e246a2e919ee6893ae6e65b3844ec
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-04-02 23:26:24 +0800

    custom_count

--- count_without_count_function.py
+++ count_without_count_function.py
@@ -1,2 +1,10 @@
 #use custom function
+def custom_count(s, sub):
+    count = 0
+    sub_len = len(sub)
+    for i in range(len(s) - sub_len + 1):
+        if s[i:i + sub_len] == sub:
+            count += 1
+    return count
+
 #Print results without using the count() function
