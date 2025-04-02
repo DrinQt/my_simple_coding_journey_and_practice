deea89446017c1e3723060a69fa5e57056c02c2a
Aldrin Paul F. Reyes <aldrinreyeslol694@gmail.com>
2025-04-02 23:42:27 +0800

    custom_rindex

--- rindex_without_rindex_function.py
+++ rindex_without_rindex_function.py
@@ -1,2 +1,9 @@
 #use custom_rindex function to find the last occurrence of a substring in a string
+def custom_rindex(s, sub):
+    sub_len = len(sub)
+    for i in range(len(s) - sub_len, -1, -1):
+        if s[i:i + sub_len] == sub:
+            return i
+    raise ValueError(f"'{sub}' is not in string")
+
 #print result
